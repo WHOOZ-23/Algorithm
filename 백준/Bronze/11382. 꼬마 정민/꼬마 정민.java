9776f3c5e37384bc27f7ba964bc367839821c77e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2023-11-16 18:38:47 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		long a, b, c;
+		
+		a = scanner.nextLong();
+		b = scanner.nextLong();
+		c = scanner.nextLong();
+		
+		System.out.println(a + b + c);
+	}
+
+}

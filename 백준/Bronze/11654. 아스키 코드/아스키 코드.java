fad1bd13050f6eada2305e3f84616aac2d45875d
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2023-12-01 10:39:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int str = scanner.next().charAt(0);
+		
+		System.out.println(str);
+		
+		scanner.close();
+	}
+}

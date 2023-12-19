36bc22277b98aeeffcc8e341b07ae4a46d33aea2
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2023-12-20 01:53:20 +0900

    [Bronze III] Title: 최댓값, Time: 236 ms, Memory: 18488 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -6,9 +6,9 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		
 		int[][] arr = new int[9][9];
-		int max = Integer.MIN_VALUE;
-		int row = 0;
-		int col = 0;
+		int max = 0;
+		int row = 1;
+		int col = 1;
 		
 		for(int i=0; i<9; i++) {
 			for(int j=0; j<9; j++) {

e474363202836462566f3f43e540f451204225f2
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2023-12-25 15:09:57 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 308 ms, Memory: 19460 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -8,6 +8,7 @@ public class Main {
 		int T = scanner.nextInt();
 		
 		int[] C = new int[T];
+		int[][] coins = new int[T][4];
 		
 		for(int i=0; i<T; i++) {
 			
@@ -15,32 +16,30 @@ public class Main {
 			
 			while(1<=C[i] && C[i]<=500) {
 				
-				int Quarter = 0;
-				int Dime = 0;
-				int Nickel = 0;
-				int Penny = 0;
-				
 				if(C[i]>=25) {
-					Quarter = C[i]/25;
-					C[i] -= Quarter*25;
+					coins[i][0] = C[i]/25;
+					C[i] -= coins[i][0]*25;
 				}
 				
 				if(25>C[i] && C[i]>=10) {
-					Dime = C[i]/10;
-					C[i] -= Dime*10;
+					coins[i][1] = C[i]/10;
+					C[i] -= coins[i][1]*10;
 				}
 				
 				if(10>C[i] && C[i]>=5) {
-					Nickel = C[i]/5;
-					C[i] -= Nickel*5;
+					coins[i][2] = C[i]/5;
+					C[i] -= coins[i][2]*5;
 				}
 				
 				if(5>C[i] && C[i]>=1) {
-					Penny = C[i];
-					C[i] -= Penny;
+					coins[i][3] = C[i];
+					C[i] -= coins[i][3];
 				}
-				System.out.print(Quarter + " " + Dime + " " + Nickel + " " + Penny + "\n");
 			}
+		}
+		
+		for(int j=0; j<T; j++) {
+			System.out.print(coins[j][0] + " " + coins[j][1] + " " + coins[j][2] + " " + coins[j][3] + "\n");
 		}
 		scanner.close();
 	}

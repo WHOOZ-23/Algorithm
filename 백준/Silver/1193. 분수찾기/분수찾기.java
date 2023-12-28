43e982b9173f383ef88b55dc7c7dcfbb4399669b
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2023-12-28 11:25:55 +0900

    [Silver V] Title: 분수찾기, Time: 228 ms, Memory: 18600 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int X = scanner.nextInt();
+		
+		int i = 1;
+		int a = 0;
+		int b = 0;
+		
+		while(X > i) {
+			X -= i;
+			i++;
+		}
+		
+		if(i%2 == 0) {
+			a = X;
+			b = i - X + 1;
+		}
+		
+		if(i%2 == 1) {
+			a = i - X + 1;
+			b = X;
+		}
+		
+		System.out.println(a + "/" + b);
+		
+		scanner.close();
+	}
+}

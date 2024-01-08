4e13d8e39a27d5e3c9365d9718928da226ea5718
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-08 23:51:17 +0900

    [Bronze III] Title: 네 번째 점, Time: 228 ms, Memory: 18384 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -5,18 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		int[][] square = new int[3][2];
+		int x1 = scanner.nextInt();
+		int y1 = scanner.nextInt();
+		int x2 = scanner.nextInt();
+		int y2 = scanner.nextInt();
+		int x3 = scanner.nextInt();
+		int y3 = scanner.nextInt();
 		
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<2; j++) {
-				square[i][j] = scanner.nextInt();
-			}
-		}
-		
-		int x = square[0][0]^square[1][0]^square[2][0];
-		int y = square[0][1]^square[1][1]^square[2][1];
-		
-		System.out.println(x + " " + y);
+		System.out.println((x1^x2^x3) + " " + (y1^y2^y3));
 		
 		scanner.close();
 	}

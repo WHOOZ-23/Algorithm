27891b667c0dc2f4d603c2bdb21796b3e319c7ef
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-08 15:35:43 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -10,15 +10,9 @@ public class Main {
 		int w = scanner.nextInt();
 		int h = scanner.nextInt();
 		
-		int minX = w;
-		int minY = h;
-		
 		if((1<=w && w<=1000) && (1<=h && h<=1000)) {
-			if((1<=x && x<=w-1) && (1<=y && y<=h-1)) {
-				minX = x < w-x ? x : w-x;
-				minY = y < h-y ? y : h-y;
-				
-				System.out.println(Math.min(minX, minY));
+			if((1<=x && x<=w-1) && (1<=y && y<=h-1)) {				
+				System.out.println(Math.min(Math.min(x, w-x), Math.min(y, h-y)));
 			}
 		}
 		scanner.close();

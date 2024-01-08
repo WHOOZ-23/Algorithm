f97b6c051db18cbac05ad2d54b2c978633821568
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-08 23:43:01 +0900

    [Bronze III] Title: 네 번째 점, Time: 232 ms, Memory: 18352 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -5,26 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		int x1 = scanner.nextInt();
-		int y1 = scanner.nextInt();
-		int x2 = scanner.nextInt();
-		int y2 = scanner.nextInt();
-		int x3 = scanner.nextInt();
-		int y3 = scanner.nextInt();
+		int[][] square = new int[3][2];
 		
-		if(x1==x2 && y1==y3) {
-			System.out.println(x3 + " " + y2);
-		} else if(x1==x3 && y1==y2) {
-			System.out.println(x2 + " " + y3);
-		} else if(x2==x3 && y1==y2) {
-			System.out.println(x1 + " " + y3);
-		} else if(x2==x3 && y1==y3) {
-			System.out.println(x1 + " " + y2);
-		} else if(x1==x2 && y2==y3) {
-			System.out.println(x3 + " " + y1);
-		} else if(x1==x3 && y2==y3) {
-			System.out.println(x2 + " " + y1);
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<2; j++) {
+				square[i][j] = scanner.nextInt();
+			}
 		}
+		
+		int x = square[0][0]^square[1][0]^square[2][0];
+		int y = square[0][1]^square[1][1]^square[2][1];
+		
+		System.out.println(x + " " + y);
+		
 		scanner.close();
 	}
 }

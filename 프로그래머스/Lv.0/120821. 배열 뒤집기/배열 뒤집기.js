e6b6a85ecc0228862caaeeb4f957dd1df836410e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-13 04:32:35 +0900

    [level Lv.0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/Lv.0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,9 +1,3 @@
 function solution(num_list) {
-    let arr = [];
-    
-    for(let i=num_list.length-1; i>=0; i--) {
-        arr.push(num_list[i]);
-    }
-    
-    return arr;
+    return num_list.reverse();
 }

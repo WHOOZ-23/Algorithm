a3346191c87da9f99912fe6bbe98defb0502d6c3
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-13 05:21:32 +0900

    [level Lv.0] Title: 최빈값 구하기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/Lv.0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,20 +1,22 @@
 function solution(array) {
+  if (array.length === 0) {
+    return -1;
+  }
+    
   const frequencyMap = array.reduce((acc, num) => {
     acc[num] = (acc[num] || 0) + 1;
     return acc;
   }, {});
 
   let maxFrequency = 0;
-  let mode = -1;
-
+  let modes = [];
   for (const num in frequencyMap) {
     if (frequencyMap[num] > maxFrequency) {
       maxFrequency = frequencyMap[num];
-      mode = parseInt(num);
+      modes = [parseInt(num)];
     } else if (frequencyMap[num] === maxFrequency) {
-      mode = -1;
+      modes.push(parseInt(num));
     }
   }
-
-  return mode;
+  return modes.length > 1 ? -1 : modes[0];
 }

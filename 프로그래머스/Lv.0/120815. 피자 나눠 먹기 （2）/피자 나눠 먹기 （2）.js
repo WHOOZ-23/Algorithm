ad66f108e7a29d62658fd35def75ff8896b02a1c
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-14 02:04:31 +0900

    [level Lv.0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/Lv.0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -2,11 +2,10 @@ function solution(n) {
     let x = 1;
     
     while(true) {
-        if((x%n===0) && (x%6===0)) {
-            return x/6;
+        if(n*x%6===0) {
+            return n*x/6;
             break;
-        } else {
-            x++;
         }
+        x++;
     }
 }

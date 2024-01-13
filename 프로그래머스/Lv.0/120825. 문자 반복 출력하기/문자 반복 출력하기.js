3a11696cbd7000bc49a414096c12f70c47412013
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-14 02:36:30 +0900

    [level Lv.0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/Lv.0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,10 +1,3 @@
 function solution(my_string, n) {
-    let str = my_string.split("");
-    let arr = [];
-    for(let i=0; i<str.length; i++) {
-      for(let j=0; j<n; j++) {
-        arr.push(str[i]);
-      }
-    }
-    return arr.toString().replaceAll(",", "");
+  return my_string.split("").reduce((a,b) => [...a, b.repeat(n)], "").join("");
 }

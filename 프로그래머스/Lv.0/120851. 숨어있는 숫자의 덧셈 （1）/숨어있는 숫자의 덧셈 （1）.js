3dae479850d5b5777a6768ab5f80058098a4750d
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-15 03:33:36 +0900

    [level Lv.0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/Lv.0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {    
-    return Array.from(my_string.replace(/[^0-9]/g, "")).reduce((a, b) => a + Number(b), 0);
+    return [...my_string.replace(/[^0-9]/g, "")].reduce((a, b) => a + Number(b), 0);
 }

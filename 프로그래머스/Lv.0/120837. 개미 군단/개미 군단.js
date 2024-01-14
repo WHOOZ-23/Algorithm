b73bc8af26dbbf8ac6266e8bbaea027e81b17571
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-15 04:30:27 +0900

    [level Lv.0] Title: 개미 군단, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/Lv.0/120837. 개미 군단/개미 군단.js
@@ -1,17 +1,3 @@
 function solution(hp) {
-    let ant = 0;
-    
-    if(hp>=5) {
-        ant += ~~(hp/5);
-        hp %= 5;
-    }
-    if(hp>=3) {
-        ant += ~~(hp/3);
-        hp %= 3;
-    }
-    if(hp>=1) {
-        ant += hp;
-    }
-        
-    return ant;
+    return ~~(hp/5) + ~~((hp%5)/3) + ~~((hp%5)%3);
 }

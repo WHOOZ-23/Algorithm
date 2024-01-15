45fe348fc41e45a1f2da06db59fefed1ad92e711
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-15 16:08:45 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 200 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -5,13 +5,8 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		
 		int n = scanner.nextInt();
-		int sum = 0;
 		
-		for(int i=0; i<n; i++) {
-			sum += 1;
-		}
-		
-		System.out.println(sum);
+		System.out.println(n);
 		System.out.println(1);
 		
 		scanner.close();

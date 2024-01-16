cfc8fddbfaa9dbeeca8c41fda5bce01b5c79facd
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-17 02:17:58 +0900

    [level 0] Title: k의 개수, Time: 7.40 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -1,3 +1,3 @@
 function solution(i, j, k) {
-    return Array.from({length:j-i+1}, (_, index) => index+i).join("").split("").filter((x) => x===k.toString()).length;
+    return Array.from({length:j-i+1}, (_, index) => index+i).join("").split("").filter((x) => +x===k).length;
 }

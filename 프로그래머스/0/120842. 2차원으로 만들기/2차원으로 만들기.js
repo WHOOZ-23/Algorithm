4b026bc50118f5b43a4b29b4ed613d2fb55e8e07
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-16 23:52:23 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,3 @@
 function solution(num_list, n) {
-    let arr = [];
-    while(num_list.length) {
-      arr.push(num_list.splice(0, n));
-    }
-    return arr;
+    return Array(num_list.length/n).fill([]).map(() => num_list.splice(0, n));
 }

55f80704e3f8699c7df94ba65aceaa8f9f0df083
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-18 04:08:48 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,11 +1,1 @@
-function solution(my_string) {
-    let numbers = my_string.split(" ").map((x) => isNaN(x) ? x : +x);
-    let result = numbers[0];
-  
-    numbers.forEach((x, i) => {
-      if(x==="+") result += numbers[++i];
-      if(x==="-") result -= numbers[++i];
-    }) 
-  
-    return result;
-}
+var solution = eval;

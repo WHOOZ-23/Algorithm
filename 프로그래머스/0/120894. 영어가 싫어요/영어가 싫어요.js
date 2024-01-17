4e86b20c07975f51c47510d29b22ee93e2000447
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-18 03:33:42 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,7 +1,3 @@
 function solution(numbers) {
-    let num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-  
-    num.forEach((x, i) => numbers = numbers.replaceAll(x, i));
-  
-    return +numbers;
+    return +["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].reduce((a, b, i) => a.replaceAll(b, i), numbers);
 }

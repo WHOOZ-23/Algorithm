55dc4dc0a83cc1987d0af1674ccb9e1b2f3a276b
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-19 02:09:18 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,3 +1,3 @@
 function solution(n) {
-    return Array.from({length:200}, (x, i) => i+1).filter((x) => x%3 && !x.toString().includes("3"))[n-1];
+    return Array.from({length:n*3}, (_, i) => i+1).filter((x) => x%3 && !x.toString().includes("3"))[n-1];
 }

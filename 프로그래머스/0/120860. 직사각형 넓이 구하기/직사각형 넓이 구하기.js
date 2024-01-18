531008e23d5031e370216e1a41ba3e2a4bc8d3c0
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-18 17:30:47 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,19 +1,10 @@
 function solution(dots) {
-    let arr = [];
-    let maxX = dots[0][0], 
-        minX = dots[0][0], 
-        maxY = dots[0][1], 
-        minY = dots[0][1];
+    let x = [], y = [];
   
-    for(let i=0; i<dots.length-1; i++) {
-        maxX = maxX < dots[i+1][0] ? dots[i+1][0] : maxX;
-        minX = minX > dots[i+1][0] ? dots[i+1][0] : minX;
-        maxY = maxY < dots[i+1][1] ? dots[i+1][1] : maxY;
-        minY = minY > dots[i+1][1] ? dots[i+1][1] : minY;
+    for(let dot of dots) {
+        x.push(dot[0]);
+        y.push(dot[1]);
     }
-    
-    arr[0] = maxX - minX;
-    arr[1] = maxY - minY;
   
-    return arr[0]*arr[1];
+    return (Math.max(...x)-Math.min(...x)) * (Math.max(...y)-Math.min(...y));
 }

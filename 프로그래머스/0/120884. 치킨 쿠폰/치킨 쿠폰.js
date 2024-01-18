a064b7807aa528061dc463c1c7145017d0cd1fa9
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-19 01:38:18 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,12 +1,3 @@
 function solution(chicken) {
-    let coupon = chicken, service = 0
-
-    while(coupon >= 10){
-        let newService = ~~(coupon/10)
-        service += newService
-        coupon = coupon % 10 + newService
-    }
-    
-    return service;
-
+    return ~~((chicken-1) / 9);
 }

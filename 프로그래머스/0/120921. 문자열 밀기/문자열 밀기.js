c4b33371eaa35f03986b5dc76edd3ecacbe61e2e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-19 17:32:41 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,15 +1,3 @@
 function solution(A, B) {
-    if(A===B) return 0;
-    
-    let arr = A.split("");
-    let cnt = 0;
-    
-    for(let i=0; i<arr.length; i++) {
-        let temp = arr.pop();
-        arr.unshift(temp);
-        cnt++;
-        if(arr.join("")===B) return cnt;
-    }
-    
-    return -1;
+    return (B+B).indexOf(A);
 }

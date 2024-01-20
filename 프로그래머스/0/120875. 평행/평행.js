0d2bc61f1376339d7fc0b703e403267650275ca7
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-21 04:24:22 +0900

    [level 0] Title: 평행, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.js
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -1,11 +1,9 @@
 function solution(dots) {
     let [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots;
-    let answer = 0;
     
-    answer += Math.abs(y1-y2)/Math.abs(x1-x2) === Math.abs(y3-y4)/Math.abs(x3-x4) ? 1 : 0;
-    answer += Math.abs(y1-y3)/Math.abs(x1-x3) === Math.abs(y2-y4)/Math.abs(x2-x4) ? 1 : 0;
-    answer += Math.abs(y1-y4)/Math.abs(x1-x4) === Math.abs(y2-y3)/Math.abs(x2-x3) ? 1 : 0;
+    if(Math.abs(y1-y2)/Math.abs(x1-x2) === Math.abs(y3-y4)/Math.abs(x3-x4)) return 1;
+    if(Math.abs(y1-y3)/Math.abs(x1-x3) === Math.abs(y2-y4)/Math.abs(x2-x4)) return 1;
+    if(Math.abs(y1-y4)/Math.abs(x1-x4) === Math.abs(y2-y3)/Math.abs(x2-x3)) return 1;
     
-    
-    return answer>0 ? 1 : 0;
+    return 0;
 }

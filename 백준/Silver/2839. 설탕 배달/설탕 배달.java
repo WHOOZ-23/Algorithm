49ec9df86284dfcae2568a472655e90f7f8524b0
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-21 18:06:27 +0900

    [Silver IV] Title: 설탕 배달, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		int cnt = 0;
+		
+		if(N==4 || N==7) {
+			cnt = -1;
+		} else if(N%5==1 || N%5==3) {
+			cnt = (N/5)+1;
+		} else if(N%5==2 || N%5==4) {
+			cnt = (N/5)+2;
+		} else if(N%5==0) {
+			cnt = N/5;
+		}
+		
+		System.out.println(cnt);
+		
+		scanner.close();
+	}
+}

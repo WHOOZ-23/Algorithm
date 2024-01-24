2103fdc39fce458db6253f301bedcb2b687497f9
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-25 02:52:26 +0900

    [level 0] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -2,16 +2,10 @@ function solution(code) {
     let ret = "";
     let mode = 0;
     
-    [...code].reduce((acc, cur, i) => {
-        if(mode===0) {
-            if(cur==="1") mode = 1;
-            else if(i%2===0) ret += cur;
-        } else {
-            if(cur==="1") mode = 0;
-            else if(i%2) ret += cur;
-        }
-        return acc;
-    }, [])
+    [...code].forEach((x, i) => {
+        if(x==="1") mode = mode===0 ? 1 : 0;
+        if(x!=="1" && i%2===mode) ret += x;
+    })
     
     return ret.length>0 ? ret : "EMPTY";
 }

6057ac887530d309241850afb65bd856530774a1
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-25 00:53:41 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,5 +1,5 @@
 function solution(a, b, c) {
-    if(a===b && b===c && c===a) {
+    if(a===b && b===c) {
         return 27*Math.pow(a, 6);
     } else if(a===b || b===c || c===a) {
         return (a+b+c)*(a*a+b*b+c*c);

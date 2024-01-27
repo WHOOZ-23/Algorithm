f15e68e670124ff643509e1b178f11c7f6106903
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-27 23:59:28 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,9 +1,6 @@
-function solution(n) {
-    let arr = [n];
-    
-    while(n!==1) {
-        arr.push(n%2 ? n=3*n+1 : n/=2);
-    }
-    
-    return arr;
+function solution(n, arr = []) {
+    arr.push(n);
+    if (n===1) return arr;
+    if (n%2) return solution(3*n+1, arr);
+    return solution(n/2, arr);
 }

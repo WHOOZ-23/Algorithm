2b40ca8c7a7b5c18f7ae90100ed5ca86ec4889c4
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-30 02:46:05 +0900

    [level 0] Title: 세로 읽기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,10 +1,3 @@
 function solution(my_string, m, c) {
-    let arr = [];
-    let str = [...my_string];
-    
-    for(let i=0; i<my_string.length/m; i++) {
-        arr.push(str.splice(0, m));
-    }
-    
-    return arr.map((x, i) => x[c-1]).join("");
+    return [...my_string].filter((_, i) => i%m===c-1).join("");
 }

e87c6f42b1b2bb26eb920d7f66b85b037426478c
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-30 02:03:50 +0900

    [level 0] Title: qr code, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.js
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -1,4 +1,3 @@
 function solution(q, r, code) {
-    // return [...code].filter((x, i) => i%q===r).join("");
-    return [...code].reduce((a, b, i) => i%q===r ? a+b : a, "");
+    return [...code].map((x, i) => i%q===r ? x : "").join("");
 }

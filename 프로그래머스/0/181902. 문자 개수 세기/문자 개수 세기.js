6174d3a21c115a3447f4de2aa48c37b36c070f57
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-30 12:30:16 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,5 +1,6 @@
 function solution(my_string) {
+    const alphabet = Array(52).fill().map((_, i) => i<=25 ? String.fromCharCode(i+65) : String.fromCharCode(i+71));
     let arr = Array(52).fill(0);
-    [...my_string].map((x) => x.charCodeAt()<=90 ? arr[x.charCodeAt()-65]++ : arr[x.charCodeAt()-71]++)
+    [...my_string].map((x) => arr[alphabet.indexOf(x)]++);
     return arr;
 }

45bac20ad812afc82848d38f47adabadbb9acc09
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-30 10:34:38 +0900

    [Bronze II] Title: 대표값2, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int[] arr = new int[5];
+		int sum = 0;
+		
+		for(int i=0; i<5; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int a:arr) {
+			sum+=a;
+		}
+		
+		System.out.println(sum/5);
+		System.out.println(arr[2]);
+		
+		scanner.close();
+	}
+}

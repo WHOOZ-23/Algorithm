f00fa33b74f28df87754843e71909f5629cb7e30
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-01-31 12:32:50 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.45 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,7 +1,5 @@
 function solution(arr, intervals) {
     const [[a1 ,b1], [a2, b2]] = intervals;
-    const arr1 = [...arr].slice(a1, b1+1);
-    const arr2 = [...arr].slice(a2, b2+1);
     
-    return [...arr1, ...arr2];
+    return [...arr.slice(a1, b1+1), ...arr.slice(a2, b2+1)];
 }

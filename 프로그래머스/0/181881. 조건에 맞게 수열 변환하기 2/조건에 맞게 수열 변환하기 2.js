ddae59cb92b9bc88873aeeb267c8056d9f7c7400
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-03 18:39:14 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,13 +1,10 @@
 function solution(arr) {
-    let arr1 = arr;
-    for(let i=0;;i++) {
-        let arr2 = arr1.map((x) => 50<=x ? (x%2 ? x : x/2) : (x%2 ? x*2+1 : x));
-        
-        if(arr1.every((x, i) => x===arr2[i])) {
-            return i;
-            break;
-        } else {
-            arr1 = arr2;
-        }
+    let i=0;
+    
+    while(!(arr.every((x) => (x>=50 && x%2) || (x<50 && x%2===0)))) {
+        arr = arr.map((x) => 50<=x ? (x%2 ? x : x/2) : (x%2 ? x*2+1 : x));
+        i++;
     }
+    
+    return i;
 }

5a041ae130cd76c99214dd5c6414f93d5316dc54
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-03 19:11:47 +0900

    [level 0] Title: 1로 만들기, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,20 +1,3 @@
 function solution(num_list) {
-    let i=0;
-    while(!(num_list.every((x) => x===1))) {
-        num_list = num_list.map((x) => {
-            if(!(x===1)) {
-                if(x%2) {
-                    i++;
-                    return (x-1)/2;
-                } 
-                
-                if(x%2===0) {
-                    i++;
-                    return x/2;
-                }
-            }
-            return x;
-        });
-    }
-    return i;
+    return num_list.map((x) => x.toString(2).length-1).reduce((a, b) => a+b);
 }

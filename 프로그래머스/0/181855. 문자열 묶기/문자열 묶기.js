0af9ee3076569b82314a7c467462d033108de53a
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-07 16:54:41 +0900

    [level 0] Title: 문자열 묶기, Time: 2.78 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,8 +1,6 @@
 function solution(strArr) {
-    const cnt = strArr.map((x) => x.length);
-    const result = {}
+    let cnt = Array(31).fill(0);
+    strArr.map((x) => cnt[x.length]++);
     
-    cnt.forEach((x) => result[x] = (result[x] || 0) + 1);
-    
-    return Math.max(...Object.values(result));
+    return Math.max(...cnt);
 }

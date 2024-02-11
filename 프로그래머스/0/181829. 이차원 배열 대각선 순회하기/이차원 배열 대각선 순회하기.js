fb059119d3f036060a0bedf14b29c9fa8dd3d61a
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-11 21:56:57 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
@@ -1,3 +1,3 @@
 function solution(board, k) {
-    return board.map((x, i) => x.filter((y, j) => i+j<=k)).flat().reduce((a, b) => a+b);
+    return board.flatMap((x, i) => x.filter((y, j) => i+j<=k)).reduce((a, b) => a+b);
 }

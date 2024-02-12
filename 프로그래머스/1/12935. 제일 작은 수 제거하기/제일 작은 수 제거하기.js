f2dd5080ad5ab808b83bd87b7ea8a68ee05b3348
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-13 02:17:45 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,4 +1,4 @@
 function solution(arr) {
-    const result = arr.filter((x) => x!==Math.min(...arr));
-    return result.length ? result : [-1];
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
+    return arr.length ? arr : [-1];
 }

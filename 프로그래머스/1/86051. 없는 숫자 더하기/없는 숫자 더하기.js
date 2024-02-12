7670310d2f11aff13c804d38e3ead7e72414716a
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-13 01:37:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,3 @@
 function solution(numbers) {
-    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    return arr.reduce((a, b) => !numbers.includes(b) ? a+b : a, 0);
+    return 45 - numbers.reduce((a, b) => a+b);
 }

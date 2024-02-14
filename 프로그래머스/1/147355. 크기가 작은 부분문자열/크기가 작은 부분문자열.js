3d6e8a3b553f47878b2383b00190a352bf7b13f5
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-14 12:48:00 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,3 +1,9 @@
 function solution(t, p) {
-    return [...t].reduce((a, b, i) => i<=t.length-p.length ? [...a, [...t].splice(i, p.length).join("")] : a, []).filter((x) => x<=+p).length;
+    let arr = [];
+    
+    for(let i=0; i<t.length-p.length+1; i++) {
+        arr.push([...t].splice(i, p.length).join(""));
+    }
+    
+    return arr.filter((x) => x<=+p).length;
 }

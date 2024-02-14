d8c49765155bd25cfa6d7da7a1cde74fe37572a0
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-15 01:56:01 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,10 +1,8 @@
 function solution(d, budget) {
-    const result = d.sort((a, b) => a-b).filter((x) => {
+    return d.sort((a, b) => a-b).filter((x) => {
         if(budget-x>=0) {
             budget-=x;
             return x;
         }
-    });
-    
-    return result.length;
+    }).length;
 }

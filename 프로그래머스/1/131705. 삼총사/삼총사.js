3f1c8cbb450a334ea49f45b7450b2202771355af
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-16 00:32:21 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,14 +1,14 @@
 function solution(number) {
-    let arr = [];
+    let answer = 0;
     
     for(let i=0; i<number.length-2; i++) {
         for(let j=i+1; j<number.length-1; j++) {
             for(let k=j+1; k<number.length; k++) {
-                let index = [number[i], number[j], number[k]];
-                arr.push(index);
+                let result = number[i] + number[j] + number[k];
+                if(result===0) answer++;
             }
         }
     }
     
-    return arr.map((x) => x.reduce((a, b) => a+b)).filter((x) => x===0).length;
+    return answer;
 }

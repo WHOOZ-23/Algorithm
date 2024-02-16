adee414b924c3554447761b53525c3b224439441
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-16 17:09:19 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,18 +1,7 @@
 function solution(s) {
-    const n = {
-        "zero": 0,
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
-    }
+    const n = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
-    Object.keys(n).forEach((x) => s = s.replaceAll(x, n[x]));
+    n.map((x, i) => s = s.replaceAll(x, i));
     
     return +s;
 }

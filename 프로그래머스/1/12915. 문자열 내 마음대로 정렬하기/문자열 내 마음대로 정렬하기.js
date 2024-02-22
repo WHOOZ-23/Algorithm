12664daa6fdb20b9e8c24a42a4a22515e66c62d1
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-22 12:06:06 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 9.35 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,7 +1,3 @@
 function solution(strings, n) {
-    return strings.sort((a, b) => {
-        if(a[n]!==b[n]) return a[n]>b[n] ? 1 : -1;
-        
-        return a>b ? 1 : -1;
-    });
+    return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
 }

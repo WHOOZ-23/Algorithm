54aba30020666b53ae307d8d5d698ed5406c2ec5
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-24 18:18:45 +0900

    [Silver V] Title: 창문 닫기, Time: 200 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		
+		System.out.println((int)Math.floor(Math.sqrt(N)));	
+		
+		scanner.close();
+	}
+}

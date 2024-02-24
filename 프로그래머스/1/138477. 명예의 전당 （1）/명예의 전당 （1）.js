d2eb505ee2b0520cccef1584ca97b166b50a4f94
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-25 01:46:59 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,14 +1,9 @@
 function solution(k, score) {
-    const queue = [];
+    let result = [];
     
     return score.reduce((a, b) => {
-        queue.push(b);
-        queue.sort((a, b) => a-b);
-        
-        if(k<queue.length) queue.shift();
-        
-        a.push(queue[0]);
-        
-        return a;
+        result.push(b);
+        result = result.sort((a, b) => b-a).slice(0, k);
+        return [...a, Math.min(...result)];
     }, []);
 }

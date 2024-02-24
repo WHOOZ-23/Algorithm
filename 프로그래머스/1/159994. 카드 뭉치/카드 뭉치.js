c51577a97c680f46acee33e9cfc9b2f62c69a69c
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-25 03:20:20 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,8 +1,9 @@
 function solution(cards1, cards2, goal) {
+    let i = 0, j = 0;
     for(let g of goal) {
-        if(cards1[0]===g) cards1.shift();
-        else if(cards2[0]===g) cards2.shift();
+        if(g===cards1[i]) i++;
+        else if(g===cards2[j]) j++;
         else return "No";
-    }
+    };
     return "Yes";
 }

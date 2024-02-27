d439b4bd04253290c1f1f7823a5da2ed601b64c1
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-27 12:41:13 +0900

    [level 1] Title: 과일 장수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,7 +1,3 @@
 function solution(k, m, score) {
-    let sum = 0;
-    
-    score.sort((a, b) => b-a);
-    
-    return score.reduce((a, b, i) => i%m===m-1 ? a+(b*m) : a, 0);
+    return score.sort((a, b) => b-a).reduce((a, b, i) => i%m===m-1 ? a+(b*m) : a, 0);
 }

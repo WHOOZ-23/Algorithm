ca6388f8bfbd60df1e421ca93bfd8d9e5e8240cd
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-02-29 15:37:26 +0900

    [level 1] Title: 소수 만들기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,25 +1,18 @@
-function solution(nums) {
-    nums.sort((a, b) => b-a);
-    
-    const len = nums[0]+nums[1]+nums[2];
-    let arr = Array(len+1).fill(true);
-
-    arr[0] = arr[1] = false;
-
-    for(let i=2; i*i<=len; i++) {
-        if(arr[i]) {
-            for(let j=i*i; j<=len; j+=i) {
-                arr[j] = false;
-            }
-        }
+function isPrime(n) {
+    for(let i=2; i<=Math.sqrt(n); i++) {
+        if(n%i===0) return false;
     }
     
+    return true;
+}
+
+function solution(nums) {    
     let cnt = 0;
     
     for(let i=0; i<nums.length-2; i++) {
         for(let j=i+1; j<nums.length-1; j++) {
             for(let k=j+1; k<nums.length; k++) {
-                if(arr[nums[i]+nums[j]+nums[k]]) {
+                if(isPrime(nums[i]+nums[j]+nums[k])) {
                     cnt++;
                 }
             }

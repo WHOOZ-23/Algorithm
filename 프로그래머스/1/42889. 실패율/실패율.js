9a23150081dcddd83379da5aab80cf2a9c2e8e7e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-03-02 02:05:41 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,22 +1,12 @@
 function solution(N, stages) {
-    let failure = Array(N).fill(0);
     let failureRate = [];
-    let rateSort = new Map();
     
-    stages.map((x) => {
-        if(x<=N) failure[x-1]++;
-    });
-    
-    let cnt = stages.length;
-    
-    for(let i=0; i<failure.length; i++) {
-        failureRate.push(failure[i] / cnt);
-        cnt = cnt - failure[i];
+    for(let i=1; i<=N; i++) {
+        let success = stages.filter((x) => i<=x).length;
+        let failure = stages.filter((x) => x===i).length;
+        
+        failureRate.push([i, failure/success]);
     }
     
-    failureRate.forEach((x, i) => {
-        rateSort.set(i+1, x);
-    });
-    
-    return [...rateSort].sort((a, b) => b[1]-a[1]).map((x) => x[0]);
+    return failureRate.sort((a, b) => b[1]-a[1]).map((x) => x[0]);
 }

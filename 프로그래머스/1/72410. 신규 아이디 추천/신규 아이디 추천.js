ad7a11b84385eb83e45401877acd9bc781e5b714
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-03-07 03:19:02 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,13 +1,10 @@
 function solution(new_id) {
-    new_id = new_id
-        .toLowerCase()
-        .replace(/[^\w-_.]/g, '')
-        .replace(/\.+/g, '.')
-        .replace(/(^\.|\.$)/g, '')
-        .replace(/^$/, 'a')
-        .slice(0, 15).replace(/(^\.|\.$)/g, '');
-    
-    const len = new_id.length;
-    
-    return len < 3 ? new_id + new_id[len-1].repeat(3-len) : new_id;
+    return new_id.toLowerCase()
+          .replace(/[^\w-_.]/g, "")
+          .replace(/\.+/g, ".")
+          .replace(/^\.|\.$/g, "")
+          .replace(/^$/, "a")
+          .match(/^.{0,14}[^.]/)[0]
+          .replace(/^(.)$/, "$1$1$1")
+          .replace(/^(.)(.)$/, "$1$2$2");
 }

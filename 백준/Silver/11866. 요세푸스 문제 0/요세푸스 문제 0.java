415e562eb1243e183c4be98520b636b7537f05c6
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-03-08 01:52:19 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 160 ms, Memory: 16108 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Deque<Integer> deque = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<=N; i++) {
+            deque.offer(i);
+        }
+
+        sb.append("<");
+
+        while(!deque.isEmpty()) {
+            for(int i=0; i<K-1; i++) {
+                deque.offer(deque.poll());
+            }
+            sb.append(deque.poll()).append(", ");
+        }
+
+        sb.delete(sb.length()-2, sb.length());
+        sb.append(">");
+
+        System.out.println(sb.toString());
+    }
+}

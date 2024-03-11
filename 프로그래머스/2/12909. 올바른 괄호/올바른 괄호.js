12e74a8d296d779f690675df1f531839fcae7f03
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-03-12 01:34:18 +0900

    [level 2] Title: 올바른 괄호, Time: 5.73 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,15 +1,14 @@
 function solution(s){
-    let arr = [...s];
     let result = [];
     
-    arr.forEach((x) => {
-        if(x==='(') {
-            result.push(x);
+    for(let c of s) {
+        if(c==='(') {
+            result.push(c);
         } else {
             if(result[result.length-1]==='(') result.pop();
-            else result.push(x);
+            else result.push(c);
         }
-    });    
+    }
     
-    return result.length===0 ? true : false;
+    return result.length===0;
 }

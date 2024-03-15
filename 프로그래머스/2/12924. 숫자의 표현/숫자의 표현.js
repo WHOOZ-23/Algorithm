caebd141c31981e787b39f0b0097e78f02e9091e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-03-15 18:01:46 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,8 +1,11 @@
 function solution(n) {
     let cnt = 0;
+    let i = 0;
     
-    for(let i=1; i<=n; i++) {
-        if((n%i===0) && (i%2===1)) cnt++;
+    while(0<n) {
+        i++;
+        if(n%i===0) cnt++;
+        n -= i;
     }
     
     return cnt;

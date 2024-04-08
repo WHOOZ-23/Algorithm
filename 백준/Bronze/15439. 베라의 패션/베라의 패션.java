908e61b60f9b3c97d26f8ace67f9bba84cdc7cfb
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-04-08 11:56:18 +0900

    [Bronze IV] Title: 베라의 패션, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        System.out.println(N*(N-1));
+
+        scanner.close();
+    }
+}

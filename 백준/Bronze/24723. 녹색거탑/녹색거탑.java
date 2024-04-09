4ba6f5a720ef81fd8a4dfa38d4416e4a725f9cbe
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-04-09 22:37:22 +0900

    [Bronze IV] Title: 녹색거탑, Time: 204 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        System.out.println((int)Math.pow(2, N));
+
+        scanner.close();
+    }
+}

5830ecaf232e665da68ca0fef8cac86faba11a42
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-04-24 21:18:44 +0900

    [Bronze V] Title: 등록, Time: 196 ms, Memory: 17296 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println(157);
+        System.out.println("whooz");
+
+        scanner.close();
+    }
+}

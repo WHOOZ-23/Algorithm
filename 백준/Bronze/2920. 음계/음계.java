deb9d899133ad0eef53be15f2b129befcbde0780
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-05-05 00:24:55 +0900

    [Bronze II] Title: 음계, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,20 +1,30 @@
-import java.util.Scanner;
-public class Main {
-   public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int[] inputs = new int[8];
-      for (int i = 0; i < inputs.length; i++) inputs[i] = sc.nextInt();
-      sc.close();
+import java.util.*;
 
-      String output = "";
-      for (int i = 0; i < inputs.length - 1; i++) {
-         if (inputs[i] == inputs[i + 1] - 1) output = "ascending";
-         else if (inputs[i] == inputs[i + 1] + 1) output = "descending";
-         else {
-            output = "mixed";
-            break;
-         }
-      }
-      System.out.println(output);
-   }
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[8];
+
+        for(int i=0; i<8; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        String str = "";
+
+        for(int j=0; j<7; j++) {
+            if(arr[j]+1 == arr[j+1]) {
+                str = "ascending";
+            } else if(arr[j]-1 == arr[j+1]) {
+                str = "descending";
+            } else {
+                str = "mixed";
+                break;
+            }
+        }
+
+        System.out.println(str);
+
+        scanner.close();
+    }
 }

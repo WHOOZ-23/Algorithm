d68affd716b67368c0923a57c4cc6a9091585a31
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-05-13 04:21:53 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 191 ms, Memory: 47,584 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,38 +1,35 @@
 import java.util.*;
-import java.io.*;
 
 public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<T; i++) {
-			int t = Integer.parseInt(br.readLine());
-			int[] arr = new int[101];
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			for(int j=0; j<1000; j++) {
-				arr[Integer.parseInt(st.nextToken())]++;
-			}
-			
-			int max = 0;
-			int cnt = 0;
-			
-			for(int k=0; k<arr.length; k++) {
-				if(max <= arr[k]) {
-					max = arr[k];
-					cnt = k;
-				}
-			}
-			
-			sb.append("#").append(t).append(" ").append(cnt).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
+        int T = scanner.nextInt();
 
+        for(int i=0; i<T; i++) {
+            int t = scanner.nextInt();
+            int[] arr = new int[101];
+
+            for(int j=0; j<1000; j++) {
+                arr[scanner.nextInt()]++;
+            }
+
+            int max = 0;
+            int cnt = 0;
+
+            for(int k=0; k<arr.length; k++) {
+                if(max <= arr[k]) {
+                    max = arr[k];
+                    cnt = k;
+                }
+            }
+
+            sb.append("#").append(t).append(" ").append(cnt).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+        scanner.close();
+    }
 }

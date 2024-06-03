0df0c7f1cc71669abc00affa7e3e876095353039
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-06-03 23:40:29 +0900

    [level 2] Title: 피보나치 수, Time: 5.76 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    const result = [0, 1];
+    
+    for(let i=2; i<=n; i++) {
+        result.push((result[i-2] + result[i-1]) % 1234567);
+    }
+    
+    return result[n];
+}

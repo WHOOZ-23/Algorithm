38822aae645d65be55b0ca64913b2673b34ba783
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-06-04 18:02:34 +0900

    [level 2] Title: 짝지어 제거하기, Time: 56.09 ms, Memory: 46.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,10 +1,9 @@
 function solution(s){
-    const arr = s.split("");
     const result = [];
     
     
     for(let i=0; i<s.length; i++) {
-        result[result.length-1]===arr[i] ? result.pop() : result.push(arr[i]);
+        result[result.length-1]===s[i] ? result.pop() : result.push(s[i]);
     }
     
     return result[0] ? 0 : 1;

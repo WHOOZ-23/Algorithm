1b04fec852714ea37a925729576b2ee981637cb7
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-06-06 12:48:14 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,14 +1,10 @@
 function solution(brown, yellow) {
-    const sum = brown + yellow;
-    let x = 0;
-    let y = 0;
-    
-    for(let i=3; i<=sum; i++) {
-        if(sum%i === 0 && (i+sum/i)*2-4 === brown) {
-            x = i;
-            y = sum/i;
+    for(var i=0; i<=(brown+yellow)/i; i++) {
+        var x = (brown+yellow)/i;
+        if((x-2)*(i-2) === yellow) {
+            break;
         }
     }
     
-    return [x, y]
+    return [x, i];
 }

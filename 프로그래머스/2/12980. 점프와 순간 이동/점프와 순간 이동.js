d690f3fcbf8a1fbc02ff2fd86b2af6730b0edd31
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-06-07 20:25:10 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let cnt = 0;
+    
+    while(n!==0) {
+        if(n%2) {
+            cnt++;
+            n = Math.floor(n/2);
+        } else {
+            n = n/2;
+        }
+    }
+    
+    return cnt;
+}

c5767757119922bfe2a56588cb8fa23fa6ed9677
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-06-17 16:24:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,14 @@
+function solution(n, words) {
+    const list = [];
+    let result = [0, 0];
+    
+    for(let i=0; i<words.length; i++) {
+        if(0 < i && words[i-1].slice(-1) !== words[i].charAt(0) || list.includes(words[i])) {
+            return [i%n+1, Math.floor(i/n)+1];
+        }
+        
+        list.push(words[i]);
+    }
+    
+    return result;
+}

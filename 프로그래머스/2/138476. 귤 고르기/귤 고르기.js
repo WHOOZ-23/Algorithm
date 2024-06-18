d5e413a388d8184e2db50ff8265578ef3552f9bc
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-06-18 17:13:22 +0900

    [level 2] Title: 귤 고르기, Time: 78.96 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,17 +1,17 @@
 function solution(k, tangerine) {
+    let cnt = 0;
     const size = {};
     
     tangerine.map((x) => size[x] = (size[x] || 0) + 1);
     
     const sorted = Object.values(size).sort((a, b) => b-a);
     
-    let cnt = 0;
-    
-    for(var i=0; i<sorted.length; i++) {
-        cnt += sorted[i];
+    for(let s of sorted) {
+        cnt++;
         
-        if(k <= cnt) break;
+        if(s < k) k -= s;
+        else break;
     }
     
-    return i+1;
+    return cnt;
 }

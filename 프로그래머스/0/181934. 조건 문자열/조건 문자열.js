2ea2813458b4644f7c6cbaacad0e25ab16349ac0
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-06-23 23:19:08 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,11 +1,6 @@
-const operations = {
-    ">=" : (n, m) => n>=m,
-    "<=" : (n, m) => n<=m,
-    ">!" : (n, m) => n>m,
-    "<!" : (n, m) => n<m,
-};
-
 function solution(ineq, eq, n, m) {
-    const op = operations[ineq + eq];
-    return +(op(n, m));
+    if(ineq === "<" && eq === "=") return n <= m ? 1 : 0;
+    if(ineq === ">" && eq === "=") return n >= m ? 1 : 0;
+    if(ineq === "<" && eq === "!") return n < m ? 1 : 0;
+    if(ineq === ">" && eq === "!") return n >= m ? 1 : 0;
 }

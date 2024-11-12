385336a06104258e5c15ae65b96c6ec52b528a77
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-11-12 22:09:28 +0900

    [level 2] Title: 괄호 회전하기, Time: 69.31 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,26 +1,14 @@
 function solution(s) {
     let answer = 0;
     let stack1 = s.split("");
+    const brackets = { "(": ")", "{": "}", "[": "]" };
 
     for (let i = 0; i < s.length; i++) {
         let stack2 = [];
+
         for (let j = 0; j < s.length; j++) {
-            if (stack2.length) {
-                if (stack2[stack2.length - 1] === "(" && stack1[j] === ")") {
-                    stack2.pop();
-                } else if (
-                    stack2[stack2.length - 1] === "{" &&
-                    stack1[j] === "}"
-                ) {
-                    stack2.pop();
-                } else if (
-                    stack2[stack2.length - 1] === "[" &&
-                    stack1[j] === "]"
-                ) {
-                    stack2.pop();
-                } else {
-                    stack2.push(stack1[j]);
-                }
+            if (brackets[stack2[stack2.length - 1]] === stack1[j]) {
+                stack2.pop();
             } else {
                 stack2.push(stack1[j]);
             }

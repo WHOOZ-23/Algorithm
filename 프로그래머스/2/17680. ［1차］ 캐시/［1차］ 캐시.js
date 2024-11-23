c713f7dff87c0b677dd5e785063bdf7dc906dce3
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-11-24 05:30:34 +0900

    [level 2] Title: [1차] 캐시, Time: 56.39 ms, Memory: 47.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,25 +1,25 @@
 function solution(cacheSize, cities) {
-    const cache = new Map();
+    const cache = [];
     let time = 0;
 
     if (!cacheSize) return cities.length * 5;
 
-    cities.forEach((city) => {
-        if (cache.has(city.toUpperCase())) {
-            cache.delete(city.toUpperCase());
-            cache.set(city.toUpperCase(), true);
-            time++;
-        } else if (!cache.has(city.toUpperCase())) {
-            if (cache.size < cacheSize) {
-                cache.set(city.toUpperCase(), true);
-                time += 5;
-            } else if (cache.size === cacheSize) {
-                cache.delete(cache.keys().next().value);
-                cache.set(city.toUpperCase(), true);
-                time += 5;
+    for (const city of cities.map((city) => city.toLowerCase())) {
+        const index = cache.indexOf(city);
+
+        if (0 <= index) {
+            cache.splice(index, 1);
+            cache.push(city);
+            time += 1;
+        } else {
+            if (cache.length === cacheSize) {
+                cache.shift();
             }
+
+            cache.push(city);
+            time += 5;
         }
-    });
+    }
 
     return time;
 }

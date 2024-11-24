1f46062b1dd81f1ebd923cbc32f052086e64a694
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-11-24 19:16:01 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -2,14 +2,8 @@ function solution(clothes) {
     const coordination = {};
 
     clothes.forEach((cloth) => {
-        coordination[cloth[1]]
-            ? coordination[cloth[1]].push(cloth[0])
-            : (coordination[cloth[1]] = [cloth[0]]);
+        coordination[cloth[1]] = (coordination[cloth[1]] || 1) + 1;
     });
 
-    return (
-        Object.values(coordination)
-            .map((cloth) => cloth.length + 1)
-            .reduce((a, b) => a * b, 1) - 1
-    );
+    return Object.values(coordination).reduce((a, b) => a * b, 1) - 1;
 }

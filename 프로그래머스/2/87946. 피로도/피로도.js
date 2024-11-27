5703374ef03d5a721d80cef390bad4d6bda2065d
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-11-27 18:53:18 +0900

    [level 2] Title: 피로도, Time: 5.72 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,23 +1,21 @@
 function solution(k, dungeons) {
     let answer = 0;
-    const visited = Array.from({ length: dungeons.length }, (_) => false);
+    const visited = new Array(dungeons.length).fill(false);
 
-    dfs(dungeons, visited, 0, k);
+    dfs(0, k);
 
-    function dfs(dungeons, visited, cnt, k) {
+    function dfs(cnt, k) {
         answer = Math.max(answer, cnt);
 
         for (let i = 0; i < dungeons.length; i++) {
             if (!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
 
-                dfs(dungeons, visited, cnt + 1, k - dungeons[i][1]);
+                dfs(cnt + 1, k - dungeons[i][1]);
 
                 visited[i] = false;
             }
         }
-
-        return;
     }
 
     return answer;

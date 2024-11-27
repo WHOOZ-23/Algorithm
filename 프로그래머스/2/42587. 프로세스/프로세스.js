4c5a5de417fa70351271afac7a63b712791153c2
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-11-27 20:40:35 +0900

    [level 2] Title: 프로세스, Time: 0.31 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,28 +1,28 @@
 function solution(priorities, location) {
     const check = new Array(priorities.length).fill(false);
-    const result = new Map();
+    const result = [];
     let answer = 9;
     let i;
 
     while (answer) {
-        if (result.size) {
-            i = Array.from(result.keys()).pop();
+        if (result.length) {
+            i = result[result.length - 1];
         } else {
             i = 0;
         }
 
         for (; i < priorities.length; i++) {
             if (!check[i] && priorities[i] === answer) {
-                result.set(i, priorities[i]);
+                result.push(i);
                 check[i] = true;
             }
 
-            if (i === priorities.length - 1 && result.size) {
-                const loop = Array.from(result.keys()).pop();
+            if (i === priorities.length - 1 && result.length) {
+                const loop = result[result.length - 1];
 
                 for (let j = 0; j < loop; j++) {
                     if (!check[j] && priorities[j] === answer) {
-                        result.set(j, priorities[j]);
+                        result.push(j);
                         check[j] = true;
                     }
                 }
@@ -32,5 +32,5 @@ function solution(priorities, location) {
         answer -= 1;
     }
 
-    return Array.from(result.keys()).indexOf(location) + 1;
+    return result.indexOf(location) + 1;
 }

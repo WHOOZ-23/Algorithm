f4cc61448e1543c4c7de0c5a7c01e28eaf7f3897
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-11-28 16:49:14 +0900

    [level 2] Title: 전화번호 목록, Time: 132.14 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,14 +1,7 @@
 function solution(phone_book) {
-    return !phone_book
-        .sort()
-        .map((phone, i) => {
-            if (i < phone_book.length - 1) {
-                if (phone_book[i + 1].startsWith(phone)) {
-                    return 1;
-                }
-            }
-
-            return 0;
-        })
-        .reduce((a, b) => a + b, 0);
+    return !phone_book.sort().some((phone, i) => {
+        if (i < phone_book.length - 1) {
+            return phone_book[i + 1].startsWith(phone);
+        }
+    });
 }

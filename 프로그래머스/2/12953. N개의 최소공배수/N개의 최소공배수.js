ccef08abb2afc8ca3feb3dce11a6b062a84293f1
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-12-03 02:28:24 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,19 +1,7 @@
 function solution(arr) {
-    let answer;
-
-    for (let i = 0; i < arr.length - 1; i++) {
-        let gcd = getGCD(arr[i], arr[i + 1]);
-        answer = Math.floor((arr[i] * arr[i + 1]) / gcd);
-        arr[i + 1] = answer;
-    }
-
-    return answer;
+    return arr.reduce((a, b) => (a * b) / gcd(a, b));
 }
 
-function getGCD(a, b) {
-    if (a % b === 0) {
-        return b;
-    } else {
-        return getGCD(b, a % b);
-    }
+function gcd(a, b) {
+    return a % b ? gcd(b, a % b) : b;
 }

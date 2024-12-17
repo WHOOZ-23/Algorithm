036870f3737c0ce0c37769b1ffeb2537113d01a9
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-12-18 02:09:22 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.17 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,36 @@
+function solution(str1, str2) {
+    let result1 = [];
+    let result2 = [];
+
+    str1 = str1.toUpperCase();
+    str2 = str2.toUpperCase();
+
+    for (let i = 0; i < str1.length - 1; i++) {
+        result1.push(str1.substring(i, i + 2));
+    }
+
+    for (let j = 0; j < str2.length - 1; j++) {
+        result2.push(str2.substring(j, j + 2));
+    }
+
+    result1 = result1.filter((x) => /[a-z][A-Z]/gi.test(x));
+    result2 = result2.filter((x) => /[a-z][A-Z]/gi.test(x));
+
+    let intersection = 0;
+    let union = 0;
+
+    for (let i = 0; i < result1.length; i++) {
+        if (result2.includes(result1[i])) {
+            intersection++;
+            result2.splice(result2.indexOf(result1[i]), 1);
+        }
+
+        union++;
+    }
+
+    union += result2.length;
+
+    const similarity = Math.floor((intersection / union) * 65536);
+
+    return isNaN(similarity) ? 65536 : similarity;
+}

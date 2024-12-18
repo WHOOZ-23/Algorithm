26befa1fd42b4804fb1a84898c3dbcf5e310d341
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2024-12-19 01:46:27 +0900

    [level 2] Title: 타겟 넘버, Time: 24.21 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,17 @@
 function solution(numbers, target) {
     let answer = 0;
 
-    dfs(numbers[0], 1);
-    dfs(-numbers[0], 1);
+    dfs(0, 0);
 
     function dfs(x, y) {
-        if (y === numbers.length) {
+        if (y < numbers.length) {
+            dfs(x + numbers[y], y + 1);
+            dfs(x - numbers[y], y + 1);
+        } else {
             if (x === target) {
                 answer++;
             }
-            return;
         }
-
-        dfs(x + numbers[y], y + 1);
-        dfs(x - numbers[y], y + 1);
     }
 
     return answer;

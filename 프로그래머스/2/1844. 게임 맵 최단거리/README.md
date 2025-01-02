86385021d65df370a783a02817ed7bb841218f15
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-01-02 16:21:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.57 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 37.7 MB, 시간: 36.55 ms
+메모리: 55.7 MB, 시간: 8.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 02일 15:45:34
+2025년 01월 02일 16:21:33
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] maps) {
+        int[] goal = {maps.length-1, maps[0].length-1};
+        Queue<int[]> queue = new LinkedList<>();
+        int[] dxs = {0, 0, -1, 1};
+        int[] dys = {1, -1, 0, 0};
+
+        queue.offer(new int[]{0, 0, 1});
+
+        while(!queue.isEmpty()) {
+            int[] move = queue.poll();
+
+            if(move[0] == goal[0] && move[1] == goal[1]) {
+                return move[2];
+            }
+
+            for(int i=0; i<4; i++) {
+                int dx = dxs[i] + move[0];
+                int dy = dys[i] + move[1];
+
+                if(0<=dx && dx<maps.length && 0<=dy && dy<maps[0].length && maps[dx][dy]==1) {
+                    queue.offer(new int[]{dx, dy, move[2]+1});
+                    maps[dx][dy] = 0;
+                }
+            }
+        }
+
+        return -1;
+    }
+}

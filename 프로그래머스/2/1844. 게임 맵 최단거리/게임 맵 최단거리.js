b45254639b3863794ad085fcb5c4970991c7afbd
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-01-02 15:45:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 36.55 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,34 @@
+function solution(maps) {
+    const goal = [maps.length - 1, maps[0].length - 1];
+    let location = [];
+    let dxs = [0, 0, -1, 1];
+    let dys = [1, -1, 0, 0];
+
+    location.push([0, 0, 1]);
+
+    while (location.length) {
+        let [x, y, move] = location.shift();
+
+        if (x === goal[0] && y === goal[1]) {
+            return move;
+        }
+
+        for (let i = 0; i < 4; i++) {
+            let dx = dxs[i] + x;
+            let dy = dys[i] + y;
+
+            if (
+                0 <= dx &&
+                dx < maps.length &&
+                0 <= dy &&
+                dy < maps[0].length &&
+                maps[dx][dy] === 1
+            ) {
+                location.push([dx, dy, move + 1]);
+                maps[dx][dy] = 0;
+            }
+        }
+    }
+
+    return -1;
+}

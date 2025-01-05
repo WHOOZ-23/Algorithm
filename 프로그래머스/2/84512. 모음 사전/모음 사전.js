e2945f1ec022b6852e62dce4b384bf11b56d6391
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-01-05 17:25:03 +0900

    [level 2] Title: 모음 사전, Time: 1.50 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,29 @@
+function solution(word) {
+    let answer = 0;
+    const vowels = ["A", "E", "I", "O", "U"];
+
+    dfs("");
+
+    function dfs(str) {
+        if (str === word) {
+            return true;
+        }
+
+        if (str.length === 5) {
+            return false;
+        }
+
+        for (v of vowels) {
+            str += v;
+            answer++;
+
+            if (dfs(str)) {
+                return true;
+            }
+
+            str = str.substring(0, str.length - 1);
+        }
+    }
+
+    return answer;
+}

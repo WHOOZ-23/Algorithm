41b3e0537f7a5f3c111772b9a1ef3b2a010e695e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-01-05 17:33:52 +0900

    [level 2] Title: 모음 사전, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,11 +1,3 @@
 function solution(word) {
-    let answer = 0;
-    let c = [781, 156, 31, 6, 1];
-    let arr = ["A", "E", "I", "O", "U"];
-
-    for (let i = 0; i < word.length; i++) {
-        answer += arr.indexOf(word[i]) * c[i] + 1;
-    }
-
-    return answer;
+    return word.split("").reduce((a, b, i) => a + ["A", "E", "I", "O", "U"].indexOf(b) * [781, 156, 31, 6, 1][i] + 1, 0);
 }

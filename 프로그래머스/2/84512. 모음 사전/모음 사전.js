278e951c5de77e51f85f6d464a712ddf6583a090
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-01-05 17:32:01 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,21 +1,10 @@
 function solution(word) {
     let answer = 0;
-    const vowels = ["A", "E", "I", "O", "U"];
+    let c = [781, 156, 31, 6, 1];
+    let arr = ["A", "E", "I", "O", "U"];
 
-    dfs("");
-
-    function dfs(str) {
-        if (str === word) return true;
-        if (str.length === 5) return false;
-
-        for (v of vowels) {
-            str += v;
-            answer++;
-
-            if (dfs(str)) return true;
-
-            str = str.substring(0, str.length - 1);
-        }
+    for (let i = 0; i < word.length; i++) {
+        answer += arr.indexOf(word[i]) * c[i] + 1;
     }
 
     return answer;

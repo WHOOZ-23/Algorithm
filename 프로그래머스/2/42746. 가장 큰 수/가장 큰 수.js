2b61f7a73c68a6391300811f0be613db5d200403
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-01-24 17:53:56 +0900

    [level 2] Title: 가장 큰 수, Time: 312.52 ms, Memory: 46 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,8 +1,5 @@
 function solution(numbers) {
-    numbers = numbers
-        .map((x) => x.toString())
-        .sort((a, b) => a + b - (b + a))
-        .reverse();
+    numbers = numbers.map((x) => x.toString()).sort((a, b) => b + a - (a + b));
 
     return numbers[0] === "0" ? "0" : numbers.join("");
 }

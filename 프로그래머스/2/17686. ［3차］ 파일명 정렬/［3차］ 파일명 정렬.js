0418d35f24dbb5cecd8ab2729bde1a3b9021d33e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-01-25 05:59:25 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 9.79 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -0,0 +1,18 @@
+function solution(files) {
+    const regex = /^([a-zA-Z\-\.\s]+)|(\d{1,5})|(.*)$/g;
+
+    files = files.sort((a, b) => {
+        const file1 = a.toLowerCase().match(regex);
+        const file2 = b.toLowerCase().match(regex);
+
+        if (file1[0] === file2[0] && file1[1] === +file2[1]) {
+            return 0;
+        } else if (file1[0] === file2[0]) {
+            return file1[1] - file2[1];
+        } else if (file1[0] < file2[0]) {
+            return -1;
+        }
+    });
+
+    return files;
+}

12982aff42416c70319db9893c449ba4f7742639
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-02-24 22:58:44 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 184 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num = sc.nextDouble();
+
+        System.out.println((int) Math.ceil(num/5));
+
+        sc.close();
+    }
+}

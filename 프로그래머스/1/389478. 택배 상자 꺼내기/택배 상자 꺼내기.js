b3432f5e51d4856609e5f3dcc44a6b9aab650f1a
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-03-04 19:35:19 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,35 +1,22 @@
 function solution(n, w, num) {
-    let arr = [];
-    let box = 1;
-    let check = 1;
+    let arr = Array.from({ length: n % w }, (_, i) => i + 1);
+    let topline = Math.ceil(n / w);
+    let line = Math.ceil(num / w);
     let answer = 0;
 
-    for (let i = 0; i < Math.floor(n / w); i++) {
-        const line1 = Array.from({ length: w }, (_, i) => box + i);
+    if (w === 1) return n - num + 1;
 
-        check % 2 ? arr.push(line1) : arr.push(line1.reverse());
+    if (w !== 1 && n % w === 0) return topline - line + 1;
 
-        box += w;
-        check++;
+    for (let i = 0; i < w - (n % w); i++) {
+        arr.push(0);
     }
 
-    const line2 = Array.from({ length: n % w }, (_, i) => box + i);
-
-    for (let j = 0; j < w - (n % w); j++) {
-        line2.push(0);
+    if (!(topline % 2)) {
+        arr = arr.reverse();
     }
 
-    check % 2 ? arr.push(line2) : arr.push(line2.reverse());
-    arr = arr.reverse();
-    arr.map((x, i) => {
-        if (x.includes(num)) {
-            if (arr[0][x.indexOf(num)] !== 0) {
-                answer = i + 1;
-            } else {
-                answer = i;
-            }
-        }
-    });
+    answer = line % 2 ? arr[(num - 1) % w] : arr[w - ((num - 1) % w) - 1];
 
-    return answer;
+    return answer ? topline - line + 1 : topline - line;
 }

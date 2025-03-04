a13bf8a97ff472a797085ca5804ac71b4abd2e44
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-03-04 19:37:56 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,22 +1,10 @@
 function solution(n, w, num) {
-    let arr = Array.from({ length: n % w }, (_, i) => i + 1);
-    let topline = Math.ceil(n / w);
-    let line = Math.ceil(num / w);
     let answer = 0;
 
-    if (w === 1) return n - num + 1;
-
-    if (w !== 1 && n % w === 0) return topline - line + 1;
-
-    for (let i = 0; i < w - (n % w); i++) {
-        arr.push(0);
+    while (num <= n) {
+        num += 2 * (w - ((num - 1) % w) - 1) + 1;
+        answer++;
     }
 
-    if (!(topline % 2)) {
-        arr = arr.reverse();
-    }
-
-    answer = line % 2 ? arr[(num - 1) % w] : arr[w - ((num - 1) % w) - 1];
-
-    return answer ? topline - line + 1 : topline - line;
+    return answer;
 }

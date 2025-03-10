2fd83918065dd6cb72b6303d953e59f4b5b135f1
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-03-10 20:30:22 +0900

    [level 2] Title: 시소 짝꿍, Time: 24.00 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -5,17 +5,17 @@ function solution(weights) {
     weights.map((x) => (counts[x] = (counts[x] || 0) + 1));
 
     for (let i = 0; i <= 1000; i++) {
-        if (0 < counts[i]) {
+        if (counts[i]) {
             answer += (counts[i] * (counts[i] - 1)) / 2;
-            if (0 < counts[(i * 4) / 3]) {
+            if (counts[(i * 4) / 3]) {
                 answer += counts[i] * counts[(i * 4) / 3];
             }
 
-            if (0 < counts[(i * 4) / 2]) {
+            if (counts[(i * 4) / 2]) {
                 answer += counts[i] * counts[(i * 4) / 2];
             }
 
-            if (0 < counts[(i * 3) / 2]) {
+            if (counts[(i * 3) / 2]) {
                 answer += counts[i] * counts[(i * 3) / 2];
             }
         }

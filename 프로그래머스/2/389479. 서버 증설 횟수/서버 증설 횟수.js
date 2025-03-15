dfb0bcbfe515d58c7f1448f7c0366ec380050dc1
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-03-15 21:35:17 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
@@ -6,17 +6,17 @@ function solution(players, m, k) {
     for (let i = 0; i < 24; i++) {
         const need = Math.floor(players[i] / m);
 
+        if (servers[i]) {
+            total -= servers[i];
+            delete servers[i];
+        }
+
         if (total < need) {
             const add = need - total;
 
             total += add;
             answer += add;
-            servers[i + k - 1] = add;
-        }
-
-        if (servers[i]) {
-            total -= servers[i];
-            delete servers[i];
+            servers[i + k] = add;
         }
     }
 

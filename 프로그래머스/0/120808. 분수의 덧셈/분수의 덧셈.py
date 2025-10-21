1feef38de985e796cc591d196ef65c3bf2093d35
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-22 02:12:46 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,9 +1,9 @@
 import math
 
-def solution(numer1, denom1, numer2, denom2):
-    a = (numer1 * denom2) + (numer2 * denom1) 
-    b = denom1 * denom2
-    c = math.gcd(a, b)  
-    answer = [a // c, b // c]
 
-    return answer
+def solution(numer1, denom1, numer2, denom2):
+    numer = numer1 * denom2 + numer2 * denom1
+    denom = denom1 * denom2
+    gcd = math.gcd(numer, denom)
+    
+    return [numer // gcd, denom // gcd]

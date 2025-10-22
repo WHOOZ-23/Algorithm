4174f6af7afebf39f3941331be652643ea34985e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-22 17:55:43 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -6,8 +6,6 @@ def solution(array):
     mode = counter.most_common(2)
 
     if len(mode) > 1 and mode[0][1] == mode[1][1]:
-        print(-1)
         return -1
     else:
-        print(mode[0][0])
         return mode[0][0]

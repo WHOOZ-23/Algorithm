976aa6cb17e5999aaec025d69723e5e58bed71aa
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-23 18:47:34 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,12 +1,9 @@
-import math
-
-
 def solution(price):
     if price >= 500000:
-        return math.floor(price * 0.8)
+        price *= 0.8
     elif price >= 300000:
-        return math.floor(price * 0.9)
+        price *= 0.9
     elif price >= 100000:
-        return math.floor(price * 0.95)
-    else:
-        return price
+        price *= 0.95
+
+    return int(price)

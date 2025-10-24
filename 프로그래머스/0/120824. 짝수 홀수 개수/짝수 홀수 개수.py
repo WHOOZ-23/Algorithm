9e600e2dc56660aeb1e0d924ddf2be473e6afd55
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-24 15:09:34 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,10 +1,7 @@
 def solution(num_list):
-    [even, odd] = [0, 0]
+    arr = [0, 0]
 
     for num in num_list:
-        if num % 2 == 0:
-            even += 1
-        else:
-            odd += 1
+        arr[num % 2] += 1
 
-    return [even, odd]
+    return arr

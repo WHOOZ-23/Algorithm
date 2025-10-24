82638c88a4820b69ab474b038c3dd43817c39952
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-24 15:53:00 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,9 +1,9 @@
 def solution(angle):
-    if  0 < angle and angle < 90:
+    if  0 < angle < 90:
         return 1
     elif angle == 90:
         return 2
-    elif 90 < angle and angle < 180:
+    elif 90 < angle < 180:
         return 3
     elif angle == 180:
         return 4

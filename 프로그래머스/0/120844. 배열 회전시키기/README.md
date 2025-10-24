0bf6f1242b75115afd69b07ccda900a8e6627024
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-25 02:51:52 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/README.md
+++ 프로그래머스/0/120844. 배열 회전시키기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 회전시키기 - 120844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120844) 
 
 ### 성능 요약
 
-메모리: 9.2 MB, 시간: 0.00 ms
+메모리: 9.21 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 25일 02:50:09
+2025년 10월 25일 02:51:50
 
 ### 문제 설명
 
--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -2,13 +2,11 @@ from collections import deque
 
 
 def solution(numbers, direction):
+    move = deque(numbers)
+
     if direction == 'left':
-        left_move = deque(numbers)
-        left_move.rotate(-1)
-        
-        return list(left_move)
+        move.rotate(-1)
     elif direction == 'right':
-        right_move = deque(numbers)
-        right_move.rotate(1)
-        
-        return list(right_move)
+        move.rotate(1)
+
+    return list(move)

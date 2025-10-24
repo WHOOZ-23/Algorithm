c2964bc68a7766d58bc7885edff3d1217f97f1f2
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-24 16:56:45 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,6 @@
+def solution(emergency):
+    sorted_emergency = reversed(sorted(emergency))
+
+    emergency_dict = {val: rank + 1 for rank, val in enumerate(sorted_emergency)}
+
+    return [emergency_dict[i] for i in emergency]

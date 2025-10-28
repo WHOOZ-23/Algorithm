a1d8e0cf70dba5d04ab7800b236941ad0a884f12
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-29 01:56:56 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,7 +1,2 @@
 def solution(array, n):
-    lst = sorted(array, key=lambda x: abs(n - x))
-
-    if len(lst) > 1 and abs(n - lst[0]) == abs(n - lst[1]):
-        return lst[0] if lst[0] < lst[1] else lst[1]
-
-    return lst[0]
+    return sorted(array, key=lambda x: (abs(n - x), x))[0]

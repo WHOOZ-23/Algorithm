a5f30249f8db5c8ce8bb5235b18450b164f3be57
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-29 18:12:27 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.01 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,5 +1,2 @@
-import math
-
-
 def solution(my_str, n):
-    return [my_str[i * n:i * n + n] for i in range(0, math.ceil(len(my_str) / n))]
+    return [my_str[i:i + n] for i in range(0, len(my_str), n)]

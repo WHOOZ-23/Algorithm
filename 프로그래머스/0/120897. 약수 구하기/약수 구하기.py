3bcb8165a9d428eaf05064b9097ca4c701ad4479
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-29 14:36:45 +0900

    [level 0] Title: 약수 구하기, Time: 1.13 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,12 +1,2 @@
-import math
-
-
 def solution(n):
-    answer = []
-
-    for i in range(1, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            answer.append(i)
-            answer.append(n // i)
-
-    return sorted(set(answer))
+    return [i for i in range(1,n + 1) if n % i == 0]

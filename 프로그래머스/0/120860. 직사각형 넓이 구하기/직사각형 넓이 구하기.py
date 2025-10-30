202da46c04d6734b0cb6d5bf79a8032077a4571b
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-10-30 15:44:25 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,9 +1,2 @@
 def solution(dots):
-    w_list = []
-    h_list = []
-
-    for dot in dots:
-        w_list.append(dot[0])
-        h_list.append(dot[1])
-
-    return (max(w_list) - min(w_list)) * (max(h_list) - min(h_list))
+    return (max(dots)[0] - min(dots)[0]) * (max(dots)[1] - min(dots)[1])

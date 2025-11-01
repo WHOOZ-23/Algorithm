9c9f21b61e05d754684c04cc3d718c419496a68e
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-02 03:46:26 +0900

    [level 0] Title: 안전지대, Time: 0.11 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/README.md
+++ 프로그래머스/0/120866. 안전지대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 안전지대 - 120866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.22 ms
+메모리: 9.21 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 0일 1:56:37
+2025년 11월 02일 03:46:24
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,10 @@
+def solution(board):
+    for i, line in enumerate(board):
+        for j, cell in enumerate(line):
+            if cell == 1:
+                for danger_i in range(i-1, i+2):
+                    for danger_j in range(j-1, j+2):
+                        if 0 <= danger_i < len(board) and 0 <= danger_j < len(board[0]) and board[danger_i][danger_j] != 1:
+                            board[danger_i][danger_j] = 2
+                            
+    return len([l for k in board for l in k if l == 0])

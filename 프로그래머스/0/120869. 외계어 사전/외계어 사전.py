7cf815ecae8adaacecd12cc1c32fd5ef8f101c32
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-02 14:59:15 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,12 +1,6 @@
 def solution(spell, dic):
-    check_list = [0 for r in range(0, len(dic))]
+    for word in dic:
+        if sorted(spell) == sorted(word):
+            return 1
 
-    for i, word in enumerate(dic):
-        for s in spell:
-            if s not in word:
-                check_list[i] = 2
-                break
-            else:
-                check_list[i] = 1
-
-    return min(check_list)
+    return 2

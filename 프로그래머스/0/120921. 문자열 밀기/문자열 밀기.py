5dc20fa2f0f8ac921b9a659efddd956c4ba5123a
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-04 18:31:50 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,12 +1,2 @@
 def solution(A, B):
-    cnt = 0
-
-    while cnt < len(A):
-        if A == B:
-            return cnt
-
-        A = A[-1:] + A[:-1]
-
-        cnt += 1
-
-    return -1
+    return (B * 2).find(A)

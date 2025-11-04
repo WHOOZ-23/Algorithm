2d8187fa95b72521b6e2bfa1b8d49736652e6a51
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-04 18:13:20 +0900

    [level 0] Title: k의 개수, Time: 21.25 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,8 +1,2 @@
 def solution(i, j, k):
-    answer = 0
-
-    for num in range(i, j+1):
-        if str(k) in str(num):
-            answer += str(num).count(str(k))
-
-    return answer
+    return "".join(str(s) for s in range(i, j+1)).count(str(k))

798890366bf2de469a67db8aea7ed492d823f9fd
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-05 18:02:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.30 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,6 @@
+from collections import Counter
+
+
 def solution(participant, completion):
-    dict = {}
-
-    for i, person in enumerate(participant):
-        dict[person] = dict.get(person, 0) + 1
-        if i < len(completion):
-            dict[completion[i]] = dict.get(completion[i], 0) - 1
-
-    for key, value in dict.items():
-        if value > 0:
-            return key
+    people = Counter(participant) - Counter(completion)
+    return list(people.keys())[0]

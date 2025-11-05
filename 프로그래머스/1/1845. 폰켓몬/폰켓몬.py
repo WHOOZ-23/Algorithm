321227e4dd305997fc03f8d61f5705d8847f7c5a
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-05 18:45:32 +0900

    [level 1] Title: 폰켓몬, Time: 2.77 ms, Memory: 9.67 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -4,4 +4,4 @@ def solution(nums):
     for num in nums:
         ponkemon_type[num] = ponkemon_type.get(num, 0) + 1
 
-    return len(nums) // 2 if len(ponkemon_type.keys()) > len(nums) // 2 else len(ponkemon_type.keys())
+    return min(len(ponkemon_type.keys()), len(nums) // 2)

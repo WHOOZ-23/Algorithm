1bd89d00ff64bc036d98f17fc3545cf96148471f
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-06 20:26:40 +0900

    [level 2] Title: 전화번호 목록, Time: 438.68 ms, Memory: 45.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,16 @@
 def solution(phone_book):
-    phone_book.sort()
-    
-    for i, num in enumerate(phone_book):
-        if i < (len(phone_book) - 1) and phone_book[i+1].startswith(num):
-            return False
+    dict = {}
+
+    for num in phone_book:
+        dict[num] = 1
+
+    for num in phone_book:
+        temp = ""
+        
+        for n in num:
+            temp += n
+            
+            if temp in dict and num != temp:
+                return False
 
     return True

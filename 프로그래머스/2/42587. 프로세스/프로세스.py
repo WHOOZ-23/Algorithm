b6551929f74e57183a26b062e061dff9a9926b3f
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-12 03:46:40 +0900

    [level 2] Title: 프로세스, Time: 0.11 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,6 +2,7 @@ def solution(priorities, location):
     priority = max(priorities)
     point = priorities.index(priority)
     answer = []
+    cnt = 1
 
     while priority > 0:
         if len(answer):
@@ -9,13 +10,21 @@ def solution(priorities, location):
 
         for i in range(point, len(priorities)):
             if priorities[i] == priority:
+                if i == location:
+                    return cnt
+
                 answer.append(i)
+                cnt += 1
 
             if i == len(priorities) - 1:
                 for j in range(0, point):
                     if priorities[j] == priority:
+                        if j == location:
+                            return cnt
+
                         answer.append(j)
+                        cnt += 1
 
         priority -= 1
 
-    return answer.index(location) + 1
+    return 0

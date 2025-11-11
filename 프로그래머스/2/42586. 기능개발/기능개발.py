52ee35f8695293d88a61dc84558f98a7fdffdaf4
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-11 16:22:16 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,17 +1,20 @@
+import math
+
+
 def solution(progresses, speeds):
+    days = [math.ceil((100 - progress)/speed) for progress, speed in zip(progresses, speeds)]
     answer = []
+    lead = days[0]
+    cnt = 1
 
-    while len(progresses) > 0:
-        for i in range(len(progresses)):
-            progresses[i] += speeds[i]
+    for day in days[1:]:
+        if lead >= day:
+            cnt += 1
+        else:
+            answer.append(cnt)
+            lead = day
+            cnt = 1
 
-        count = 0
+    answer.append(cnt)
 
-        while len(progresses) > 0 and progresses[0] >= 100:
-            count += 1
-            progresses.pop(0)
-            speeds.pop(0)
-
-        answer.append(count)
-
-    return list(filter(lambda x: x > 0, answer))
+    return answer

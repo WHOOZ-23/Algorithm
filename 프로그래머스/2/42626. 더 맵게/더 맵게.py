63b273e1a823ced2520de398336628ea32ad5c3f
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-19 16:18:52 +0900

    [level 2] Title: 더 맵게, Time: 1895.15 ms, Memory: 48.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        if len(scoville) == 1:
+            return -1
+    
+        f = heapq.heappop(scoville)
+        s = heapq.heappop(scoville)
+        new = f + s * 2
+
+        answer += 1
+        heapq.heappush(scoville, new)
+
+    return answer

57fb021f2bd77a059795b6e7d54e9a6b97ab49ae
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2025-11-19 17:44:48 +0900

    [level 2] Title: 가장 큰 수, Time: 80.65 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,3 +1,3 @@
 def solution(numbers):
-    num = sorted(list(map(str, numbers)), key=lambda x : (x * 4)[:4], reverse=True)
+    num = sorted(list(map(str, numbers)), key=lambda x : (x * 3)[:4], reverse=True)
     return "0" if num[0] == "0" else "".join(num)

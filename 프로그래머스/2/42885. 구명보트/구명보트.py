5d5286592e1380b88637900d084ae166702587d4
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2026-03-04 02:03:48 +0900

    [level 2] Title: 구명보트, Time: 8.72 ms, Memory: 9.63 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,13 @@
+def solution(people, limit):
+    left, right = 0, len(people) - 1
+    cnt = 0
+
+    people.sort()
+
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        cnt += 1
+
+    return cnt

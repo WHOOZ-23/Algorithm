a6c15e0c39f1a5d04950afafb2f443a7e2bbed32
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2026-03-20 00:52:29 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,25 +1,9 @@
 def solution(word):
     vowels = ['A', 'E', 'I', 'O', 'U']
+    weights = [781, 156, 31, 6, 1]
     cnt = 0
 
-    def words(w):
-        nonlocal cnt
-
-        if w == word:
-            return True
-
-        if len(w) == 5:
-            return False
-
-        for v in vowels:
-            w += v
-            cnt += 1
-
-            if words(w):
-                return True
-
-            w = w[:-1]
-
-    words("")
+    for i, w in enumerate(word):
+        cnt += vowels.index(w) * weights[i] + 1
 
     return cnt

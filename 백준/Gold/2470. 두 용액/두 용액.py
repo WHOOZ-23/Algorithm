0a25ef3529ecdcb6f728ef20dc5df1ddb5b7028c
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2026-03-29 00:20:51 +0900

    [Gold V] Title: 두 용액, Time: 116 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+answer = [0, 0]
+left, right = 0, len(arr) - 1
+result = float('inf')
+
+while left < right:
+    mix = arr[left] + arr[right]
+
+    if abs(mix) < result:
+        result = abs(mix)
+        answer = [arr[left], arr[right]]
+
+    if mix < 0:
+        left += 1
+    elif mix > 0:
+        right -= 1
+    else:
+        break
+
+print(answer[0], answer[1])

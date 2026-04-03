e827c0d900728a963c002fd8ec052253aa2c7232
WHOOZ-23 <150788823+WHOOZ-23@users.noreply.github.com>
2026-04-03 21:01:50 +0900

    [Gold IV] Title: 부분합, Time: 104 ms, Memory: 42168 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,20 @@
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+
+left = 0
+total = 0
+answer = float('inf')
+
+for right in range(N):
+    total += arr[right]
+
+    while left <= right:
+        if S <= total:
+            answer = min(answer, right - left + 1)
+            total -= arr[left]
+            left += 1
+        else:
+            break
+
+
+print(answer if answer != float('inf') else 0)
